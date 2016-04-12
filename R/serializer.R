da35a610686cc050ef0d01525da421eb6cecb204
Maarten Slagter <slagter.maarten@gmail.com>
2016-04-12 03:44:59 +0200

    Bug fixes to serializer clear object functionality

--- R/serializer.R
+++ R/serializer.R
@@ -1,5 +1,6 @@
-#' Create a serializer function which takes expressions and subsequently
-#' evaluates them and stores the results if it hadn't been serialized before
+#' Create a serializer functional which takes expressions. 
+#' The serializer function will only evaluate the expression if the result could
+#' not be found in the cache folder, storing the result for future evaluations
 #'
 #' @param saveFolder location to store serialized objects
 #' @return function that can serialize objects
@@ -7,8 +8,8 @@ genSerializer <- function(saveFolder = 'saved-objects') {
   dir.create(saveFolder, showWarnings = F)
 
   mySerialize <- function(ex) {
-    o.name <- deparse(lhs(substitute(ex)))
-    o.path <- file.path(saveFolder, paste0(o.name, ".rds"))
+    o.name <- deparse(formula.tools::lhs(substitute(ex)))
+    o.path <- file.path(saveFolder, paste0(o.name, '.rds'))
 
     if (!file.exists(o.path)) {
       # Compute expression
@@ -27,20 +28,22 @@ genSerializer <- function(saveFolder = 'saved-objects') {
 clearObjects <- function(saveFolder = 'saved-objects')
   unlink(saveFolder, recursive = T)
 
+
 #' Remove single objects
 #'
 #' @param ex object to be removed
 #' @param saveFolder location of serialized objects
 clearObject <- function(ex, saveFolder = 'saved-objects') {
-  if (is.expression(ex))
-    o.name <- deparse(lhs(substitute(ex)))
-  else if (is.object(ex))
-    o.name <- deparse(substitute(merged.t.f))
-  else if (is.character(ex))
+  if (is.expression(ex)) {
+    o.name <- deparse(formula.tools::lhs(substitute(ex)))
+  } else if (is.object(ex)) {
+    o.name <- deparse(substitute(ex))
+  } else if (is.character(ex)) {
     o.name <- ex
-  else
-    stop("Do not know how to handle input of class", ex, ", ", class(ex))
+  } else {
+    stop('Do not know how to handle input of class', ex, ', ', class(ex))
+  }
 
-  o.path <- file.path(saveFolder, o.name)
-  file.remove(o.name)
+  o.path <- file.path(saveFolder, paste0(o.name, '.rds'))
+  file.remove(o.path)
 }
