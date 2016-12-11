4a1aa6eccefc2e4ffa9c15d504d11de7e055b79e
Maarten Slagter <slagter.maarten@gmail.com>
2016-12-11 21:36:52 +0100

    Bug fix and added print function

--- R/serializer.R
+++ R/serializer.R
@@ -1,6 +1,6 @@
-#' Create a serializer function which takes expressions to be evaluated only when 
+#' Create a serializer function which takes expressions to be evaluated only when
 #' their result has not been computed already.
-#' 
+#'
 #' The serializer function will only evaluate its argument, an assignment, if
 #' the assignment's result could not be found in the cache folder, storing the
 #' right hand side of the assignment in the cache folder to prevent future evaluations
@@ -9,8 +9,9 @@
 #' @return function that can serialize objects
 gen_serializer <- function(cache_loc = 'saved-objects') {
   dir.create(cache_loc, showWarnings = F)
+  force(cache_loc)
 
-  function(ex) {
+  so <- function(ex) {
     o.name <- deparse(formula.tools::lhs(substitute(ex)))
     o.path <- file.path(cache_loc, paste0(o.name, '.rds'))
 
@@ -22,8 +23,18 @@ gen_serializer <- function(cache_loc = 'saved-objects') {
     }
     invisible(assign(o.name, readRDS(file = o.path), envir = parent.frame()))
   }
+  class(so) <- c('serializer', 'function')
+  return(so)
 }
-genSerializer <- gen_serializer
+
+
+print.serializer <- function(co) {
+  loc <- environment(co)$cache_loc
+  ## Inventorise stored objects
+  so <- gsub("\\.rds$", "", list.files(loc, full.name = F))
+  print(sprintf('Cacher storing objects in: %s. Found files: %s', loc,
+                paste(so, collapse = ", ")))
+}
 
 
 #' Remove all serialized objects
@@ -37,15 +48,15 @@ clear_all_objects <- function(cache_loc = 'saved-objects') {
 #' Remove single objects
 #'
 #' @param ex object to be removed.  if \code{ex} is neither an object
-#' nor a character string, it is assumed to be an assignment in the form 
+#' nor a character string, it is assumed to be an assignment in the form
 #' \code{a <- func("value")}
 #' @param cache_loc location of serialized objects
 clear_object <- function(ex, cache_loc = 'saved-objects') {
   if (is.object(ex)) {
     o.name <- deparse(substitute(ex))
-  } else { 
+  } else {
     o.name <- deparse(formula.tools::lhs(substitute(ex)))
-  } 
+  }
 
   o.path <- file.path(cache_loc, paste0(o.name, '.rds'))
   if (file.exists(o.path)) {
@@ -57,8 +68,8 @@ clear_object <- function(ex, cache_loc = 'saved-objects') {
 
 
 #' Example long running func (for testing purposes)
-#' 
-#' 
+#'
+#'
 long_computing_func <- function() {
   Sys.sleep(1)
   return('return value')
